ec6c1893dae085d375f443ea67ee04b65e4044f5
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2021-02-19 17:03:11 -0500

    Throw together a database implementation

--- webapp/app.py
+++ webapp/app.py
@@ -1,15 +1,33 @@
 from flask import Flask
 from flask import request
+import os
+from . import db
 # from .sqli1 import login
 # from vulnerabilities.sqli1 import sqli1
 
 app = Flask(__name__)
 # app.register_blueprint(sqli1.bp)
+app.config.from_mapping(
+    # a default secret that should be overridden by instance config
+    SECRET_KEY="dev",
+    # store the database in the instance folder
+    DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+)
+
+# ensure the instance folder exists
+try:
+    os.makedirs(app.instance_path)
+except OSError:
+    pass
+
+db.init_app(app)
+
 
 @app.route("/")
 def index():
     return app.send_static_file("index.html")
-# /login
+
+
 @app.route("/vulnerabilities/login", methods=["POST"])
 def login():
     return request.form['username']
---
+++ webapp/schema.sql
@@ -0,0 +1,13 @@
+-- Initialize the database.
+-- Drop any existing data and create empty tables.
+
+DROP TABLE IF EXISTS user;
+DROP TABLE IF EXISTS post;
+
+CREATE TABLE user (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  username TEXT UNIQUE NOT NULL,
+  password TEXT NOT NULL
+);
+
+insert into user(username,password)values("administrator","password123")
