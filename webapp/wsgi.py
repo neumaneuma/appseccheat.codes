e0ff6d65fed19cef1c14a66348541d60c960abf1
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2021-03-03 11:40:00 -0500

    Use gunicorn as the WSGI server

--- webapp/wsgi.py
+++ webapp/wsgi.py
@@ -1,7 +1,8 @@
 """Application entry point."""
 from webapp import init_app
 
-app = init_app()
+# Gunicorn requires the exposed flask app to be called `application`
+application = init_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
