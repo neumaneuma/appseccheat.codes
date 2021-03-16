ebe42a087833da7fa696546c6ae8caabb21d8ae7
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2021-03-16 18:53:41 -0400

    Add route for challenge page of first sql injection

--- webapp/webapp/routes.py
+++ webapp/webapp/routes.py
@@ -3,14 +3,20 @@ from flask import Blueprint, render_template
 bp = Blueprint("routes", __name__)
 
 
-@bp.route("/", methods=["GET"])
-def index():
-    return render_template("index.html")
-
 # TODO remove this and make it a command line
 @bp.route("/reset")
 def reset_database():
     from . import database
+
     database.reset_database()
 
     return "reset"
+
+
+@bp.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+@bp.route("/sqli1", methods=["GET"])
+def sqli1():
+    return render_template("challenge.html")
