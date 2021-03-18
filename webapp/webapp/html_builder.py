9c3a713b1c0bb7ab4f0e90bd43244e3feb79bb7b
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2021-03-18 19:10:12 -0400

    DRY some more blocks and inject in dynamic links

--- webapp/webapp/html_builder.py
+++ webapp/webapp/html_builder.py
@@ -1,3 +1,13 @@
+SQLI1_LINKS = {
+    "vulnerability_source_code": "https://github.com/neumaneuma/appseccheat.codes/blob/main/webapp/webapp/vulnerabilities/sqli_login_bypass.py",
+    "vulnerability_gist": "https://gist.github.com/neumaneuma/39a853dfe14e7084ecc8ac8b304c60a3.js",
+    "exploit_source_code": "https://github.com/neumaneuma/appseccheat.codes/blob/main/exploits/sqli_login_bypass.py",
+    "exploit_gist": "https://gist.github.com/neumaneuma/2cd5ffda86a9f3beee7858fd3ee21b10.js",
+    "patch_source_code": "https://github.com/neumaneuma/appseccheat.codes/blob/main/webapp/webapp/patches/sqli_login_bypass.py",
+    "patch_gist": "https://gist.github.com/neumaneuma/0076b3c6735f6002c680415483566e6e.js",
+}
+
+
 def build_headers(introduction):
     return {
         "introduction": introduction,
--- webapp/webapp/routes.py
+++ webapp/webapp/routes.py
@@ -22,4 +22,4 @@ def index():
 @bp.route("/sqli1", methods=["GET"])
 def sqli1():
     headers = html_builder.build_headers("What is SQL injection?")
-    return render_template("sqli1.html", headers=headers)
+    return render_template("sqli1.html", headers=headers, links=html_builder.SQLI1_LINKS)
