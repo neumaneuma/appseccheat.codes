7c08f80826f63bd33b5e143cceb7dbfa5ee5d600
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2021-03-20 19:13:25 -0400

    Update gist for second sqli challenge

--- webapp/webapp/html_builder.py
+++ webapp/webapp/html_builder.py
@@ -9,11 +9,11 @@ SQLI1_LINKS = {
 
 SQLI2_LINKS = {
     "vulnerability_source_code": "https://github.com/neumaneuma/appseccheat.codes/blob/main/webapp/webapp/vulnerabilities/sqli_second_order.py",
-    "vulnerability_gist": "https://gist.github.com/neumaneuma/39a853dfe14e7084ecc8ac8b304c60a3.js",
+    "vulnerability_gist": "https://gist.github.com/neumaneuma/a96c8d6c304e94cdd343a17a7ad0a7ee.js",
     "exploit_source_code": "https://github.com/neumaneuma/appseccheat.codes/blob/main/exploits/sqli_second_order.py",
-    "exploit_gist": "https://gist.github.com/neumaneuma/2cd5ffda86a9f3beee7858fd3ee21b10.js",
+    "exploit_gist": "https://gist.github.com/neumaneuma/a258d30a2551184f00e455969d9fc413.js",
     "patch_source_code": "https://github.com/neumaneuma/appseccheat.codes/blob/main/webapp/webapp/patches/sqli_second_order.py",
-    "patch_gist": "https://gist.github.com/neumaneuma/0076b3c6735f6002c680415483566e6e.js",
+    "patch_gist": "https://gist.github.com/neumaneuma/bef037bcd02ae91e6c6ecf1aac46546d.js",
 }
 
 
