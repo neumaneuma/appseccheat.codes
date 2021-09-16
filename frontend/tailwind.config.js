6af15b8545a22f848789e0a75d63aa5567fe05f6
Bradley Neumaier￼ <neumaierbradley@icloud.com>
2021-09-16 12:14:16 -0400

    Fix UI defect

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,13 @@ module.exports = {
   purge: ["../webapp/webapp/templates/**/*.html", "../webapp/webapp/templates/**/*.j2"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-  },
+    extend: {
+      transitionProperty: {
+        'max-height': 'max-height'
+      }
+    }
+  }
+,
   variants: {
     extend: {
     },
