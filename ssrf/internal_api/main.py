bcde027c8ba7ecfc6384eac46cbe4a7fd37ceb7e
Bradley Neumaier <neumaierbradley@icloud.com>
2024-11-05 18:49:58 -0500

    ready to test?

---
+++ ssrf/internal_api/main.py
@@ -0,0 +1,25 @@
+import random
+from datetime import datetime
+
+from fastapi import FastAPI
+
+PASSWORD_RESET = (
+    "E0304F61-0E09-4200-8086-AC2C6546F56F-835A6D48-E659-4F55-8730-9636A9F55E92"
+)
+
+app = FastAPI()
+
+
+@app.get("/reset_admin_password/")
+async def simulate_reset_admin_password():
+    return f"Administrator password reset to: {PASSWORD_RESET}"
+
+
+@app.get("get_cat_coin_price_v2/")
+async def get_cat_coin_price_v2():
+    return f"Price at {datetime.now().time()} - ${random.random() * 100}"
+
+
+@app.get("get_cat_coin_price_v1/")
+async def get_cat_coin_price_v1():
+    return f"Price at {datetime.now().time()} - ${random.random() * 100}"
