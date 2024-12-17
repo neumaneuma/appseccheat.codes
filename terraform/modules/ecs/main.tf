9ba937b82868a3aec3045237e8f81b867bd5664f
Bradley Neumaier <neumaierbradley@icloud.com>
2024-12-17 13:54:07 -0500

    closer but also not working e2e

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -95,6 +95,13 @@ resource "aws_launch_template" "ecs" {
   image_id      = data.aws_ssm_parameter.ecs_optimized_ami.value
   instance_type = "t2.micro"
 
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
+    }
+  }
+
   # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/launch_container_instance.html
   # Fargate abstracts away this hackiness; EC2 instances require this.
   user_data = base64encode(<<-EOF
@@ -222,6 +229,14 @@ resource "aws_vpc_security_group_egress_rule" "allow_private_traffic" {
   ip_protocol = "-1" # semantically equivalent to all ports until i verify everything works
 }
 
+resource "aws_vpc_security_group_ingress_rule" "allow_alb_to_ecs" {
+  security_group_id            = aws_security_group.allow_tls.id
+  from_port                    = 12301
+  to_port                      = 12301
+  ip_protocol                  = "tcp"
+  referenced_security_group_id = aws_security_group.allow_tls.id # Allow traffic from the ALB's security group
+}
+
 resource "aws_lb" "main" {
   name               = "app-load-balancer"
   internal           = false
@@ -237,6 +252,7 @@ resource "aws_lb_listener" "main" {
   ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
   certificate_arn   = var.alb_certificate_arn
 
+  # this can be broken out into a `aws_lb_listener_rule` resource if it needs to be more complex
   default_action {
     type = "forward"
     forward {
@@ -265,10 +281,7 @@ resource "aws_lb_target_group" "blue" {
   vpc_id   = var.vpc_id
 }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_alb_to_ecs" {
-  security_group_id            = aws_security_group.allow_tls.id
-  from_port                    = 12301
-  to_port                      = 12301
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.allow_tls.id # Allow traffic from the ALB's security group
+resource "aws_autoscaling_attachment" "backend" {
+  autoscaling_group_name = aws_autoscaling_group.ecs.id
+  lb_target_group_arn    = aws_lb_target_group.blue.arn
 }
