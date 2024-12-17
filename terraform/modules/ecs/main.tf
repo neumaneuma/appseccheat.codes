542e59570537adea00a1cb34900cabb6ddaaa962
Bradley Neumaier <neumaierbradley@icloud.com>
2024-12-17 14:38:13 -0500

    refactor

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -7,6 +7,122 @@ resource "aws_ecr_repository" "repo" {
   }
 }
 
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_public_traffic" {
+  description       = "Allow HTTPS public traffic to the ALB"
+  count             = length(var.public_subnet_cidr_blocks)
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.public_subnet_cidr_blocks[count.index]
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_egress_rule" "alb_private_traffic" {
+  description       = "Allow ALB to communicate with the ECS instances"
+  count             = length(var.private_subnet_cidr_blocks)
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = var.private_subnet_cidr_blocks[count.index]
+  # from_port         = 443
+  #ip_protocol       = "tcp"
+  # to_port           = 12301
+  ip_protocol = "-1" # semantically equivalent to all ports until i verify everything works
+}
+
+resource "aws_security_group" "ecs_sg" {
+  name   = "ecs_sg"
+  vpc_id = var.vpc_id
+
+  tags = {
+    Name = "ecs_sg"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_alb_to_ecs" {
+  description                  = "Allow ECS instances to receive traffic from the ALB"
+  security_group_id            = aws_security_group.ecs_sg.id
+  referenced_security_group_id = aws_security_group.allow_tls.id
+  from_port                    = 12301
+  to_port                      = 12301
+  ip_protocol                  = "tcp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_ecs_to_alb" {
+  description       = "Allow ECS instances to send traffic to the ALB"
+  count             = length(var.private_subnet_cidr_blocks)
+  security_group_id = aws_security_group.ecs_sg.id
+  cidr_ipv4         = var.private_subnet_cidr_blocks[count.index]
+  # from_port         = 443
+  #ip_protocol       = "tcp"
+  # to_port           = 12301
+  ip_protocol = "-1" # semantically equivalent to all ports until i verify everything works
+}
+
+resource "aws_lb" "main" {
+  name               = "app-load-balancer"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = var.public_subnet_ids
+  security_groups    = [aws_security_group.allow_tls.id]
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.alb_certificate_arn
+
+  # this can be broken out into a `aws_lb_listener_rule` resource if it needs to be more complex
+  default_action {
+    type = "forward"
+    forward {
+      target_group {
+        arn    = aws_lb_target_group.blue.arn
+        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
+      }
+
+      # target_group {
+      #   arn    = aws_lb_target_group.green.arn
+      #   weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
+      # }
+
+      stickiness {
+        enabled  = false
+        duration = 1
+      }
+    }
+  }
+}
+
+resource "aws_lb_target_group" "blue" {
+  name     = "blue-target-group"
+  port     = 12301
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    interval            = 30
+    matcher             = "200"
+    path                = "/health"
+    port                = "traffic-port"
+    protocol            = "HTTP"
+    timeout             = 5
+    unhealthy_threshold = 2
+  }
+}
+
 resource "aws_s3_bucket" "ecs_logs" {
   bucket = var.bucket_name
 }
@@ -61,11 +177,6 @@ resource "aws_ecs_cluster" "main" {
   }
 }
 
-resource "aws_iam_instance_profile" "ecs_instance_profile" {
-  name = "ecs-instance-profile"
-  role = aws_iam_role.ecs_role.name
-}
-
 data "aws_iam_policy_document" "assume_role" {
   statement {
     effect = "Allow"
@@ -82,6 +193,11 @@ data "aws_iam_policy_document" "assume_role" {
 resource "aws_iam_role" "ecs_role" {
   name               = "ecs-role"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_role.name
 }
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
@@ -127,6 +243,12 @@ resource "aws_autoscaling_group" "ecs" {
     version = "$Latest"
   }
 }
+
+resource "aws_autoscaling_attachment" "backend" {
+  autoscaling_group_name = aws_autoscaling_group.ecs.id
+  lb_target_group_arn    = aws_lb_target_group.blue.arn
+}
+
 
 resource "aws_ecs_task_definition" "backend" {
   family                   = "backend"
@@ -196,92 +318,4 @@ resource "aws_ecs_service" "internal_api" {
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.internal_api.arn
   desired_count   = 1
-}
-
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = var.vpc_id
-
-  tags = {
-    Name = "allow_tls"
-  }
-}
-
-
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  count             = length(var.public_subnet_cidr_blocks)
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = var.public_subnet_cidr_blocks[count.index]
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
-
-
-resource "aws_vpc_security_group_egress_rule" "allow_private_traffic" {
-  count             = length(var.private_subnet_cidr_blocks)
-  security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = var.private_subnet_cidr_blocks[count.index]
-  # from_port         = 443
-  #ip_protocol       = "tcp"
-  # to_port           = 12301
-  ip_protocol = "-1" # semantically equivalent to all ports until i verify everything works
-}
-
-resource "aws_vpc_security_group_ingress_rule" "allow_alb_to_ecs" {
-  security_group_id            = aws_security_group.allow_tls.id
-  from_port                    = 12301
-  to_port                      = 12301
-  ip_protocol                  = "tcp"
-  referenced_security_group_id = aws_security_group.allow_tls.id # Allow traffic from the ALB's security group
-}
-
-resource "aws_lb" "main" {
-  name               = "app-load-balancer"
-  internal           = false
-  load_balancer_type = "application"
-  subnets            = var.public_subnet_ids
-  security_groups    = [aws_security_group.allow_tls.id]
-}
-
-resource "aws_lb_listener" "main" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-  certificate_arn   = var.alb_certificate_arn
-
-  # this can be broken out into a `aws_lb_listener_rule` resource if it needs to be more complex
-  default_action {
-    type = "forward"
-    forward {
-      target_group {
-        arn    = aws_lb_target_group.blue.arn
-        weight = lookup(local.traffic_dist_map[var.traffic_distribution], "blue", 100)
-      }
-
-      # target_group {
-      #   arn    = aws_lb_target_group.green.arn
-      #   weight = lookup(local.traffic_dist_map[var.traffic_distribution], "green", 0)
-      # }
-
-      stickiness {
-        enabled  = false
-        duration = 1
-      }
-    }
-  }
-}
-
-resource "aws_lb_target_group" "blue" {
-  name     = "blue-target-group"
-  port     = 12301
-  protocol = "HTTPS"
-  vpc_id   = var.vpc_id
-}
-
-resource "aws_autoscaling_attachment" "backend" {
-  autoscaling_group_name = aws_autoscaling_group.ecs.id
-  lb_target_group_arn    = aws_lb_target_group.blue.arn
 }
