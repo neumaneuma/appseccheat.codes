2467bfe25b749dd2f3e67ca96075fb641751ebf3
Bradley Neumaier <bradley.neumaier@pilot.com>
2024-12-27 11:18:57 -0500

    support remote endpoints in functional tests

--- backend/tests/driver.py
+++ backend/tests/driver.py
@@ -1,76 +1,90 @@
 import json
 import re
+import sys
 import time
 import uuid
 from enum import Enum
 from typing import assert_never
 
 import requests
+import urllib3
 
 from backend.passphrases import Passphrases
 
-url_prefix = "http://127.0.0.1:12301"
-verify = False
+LOCAL_SERVER_URL = "http://127.0.0.1:12301"
+REMOTE_SERVER_URL = "https://api.appseccheat.codes"
 
 
 class SQLI:
     class LoginBypass:
         class Vulnerabilities:
-            expected_data_for_login: tuple[str, int] = (
-                f"{url_prefix}/vulnerabilities/sqli1/login/",
-                200,
-            )
+            @staticmethod
+            def get_login_data(url_prefix: str) -> tuple[str, int]:
+                return (f"{url_prefix}/vulnerabilities/sqli1/login/", 200)
 
         class Patches:
-            expected_data_for_login: tuple[str, int, dict[str, str]] = (
-                f"{url_prefix}/patches/sqli1/login/",
-                403,
-                {"detail": "Login failed"},
-            )
+            @staticmethod
+            def get_login_data(url_prefix: str) -> tuple[str, int, dict[str, str]]:
+                return (
+                    f"{url_prefix}/patches/sqli1/login/",
+                    403,
+                    {"detail": "Login failed"},
+                )
 
     class SecondOrder:
         class Vulnerabilities:
-            expected_data_for_register: tuple[str, int, str] = (
-                f"{url_prefix}/vulnerabilities/sqli2/register/",
-                200,
-                "Successfully registered",
-            )
-            expected_data_for_change_password: tuple[str, int] = (
-                f"{url_prefix}/vulnerabilities/sqli2/change_password/",
-                200,
-            )
+            @staticmethod
+            def get_register_data(url_prefix: str) -> tuple[str, int, str]:
+                return (
+                    f"{url_prefix}/vulnerabilities/sqli2/register/",
+                    200,
+                    "Successfully registered",
+                )
+
+            @staticmethod
+            def get_change_password_data(url_prefix: str) -> tuple[str, int]:
+                return (f"{url_prefix}/vulnerabilities/sqli2/change_password/", 200)
 
         class Patches:
-            expected_data_for_register: tuple[str, int, str] = (
-                f"{url_prefix}/patches/sqli2/register/",
-                200,
-                "Successfully registered",
-            )
-            expected_data_for_change_password: tuple[str, int, str] = (
-                f"{url_prefix}/patches/sqli2/change_password/",
-                200,
-                "Successfully changed password",
-            )
+            @staticmethod
+            def get_register_data(url_prefix: str) -> tuple[str, int, str]:
+                return (
+                    f"{url_prefix}/patches/sqli2/register/",
+                    200,
+                    "Successfully registered",
+                )
+
+            @staticmethod
+            def get_change_password_data(url_prefix: str) -> tuple[str, int, str]:
+                return (
+                    f"{url_prefix}/patches/sqli2/change_password/",
+                    200,
+                    "Successfully changed password",
+                )
 
 
 class SSRF:
     class Webhook:
         class Vulnerabilities:
-            submit_webhook_url: str = (
-                f"{url_prefix}/vulnerabilities/ssrf1/submit_webhook/"
-            )
+            @staticmethod
+            def get_submit_webhook_url(url_prefix: str) -> str:
+                return f"{url_prefix}/vulnerabilities/ssrf1/submit_webhook/"
 
         class Patches:
-            submit_webhook_url: str = f"{url_prefix}/patches/ssrf1/submit_webhook/"
+            @staticmethod
+            def get_submit_webhook_url(url_prefix: str) -> str:
+                return f"{url_prefix}/patches/ssrf1/submit_webhook/"
 
     class LocalFileInclusion:
         class Vulnerabilities:
-            submit_api_url_url: str = (
-                f"{url_prefix}/vulnerabilities/ssrf2/submit_api_url/"
-            )
+            @staticmethod
+            def get_submit_api_url(url_prefix: str) -> str:
+                return f"{url_prefix}/vulnerabilities/ssrf2/submit_api_url/"
 
         class Patches:
-            submit_api_url_url: str = f"{url_prefix}/patches/ssrf2/submit_api_url/"
+            @staticmethod
+            def get_submit_api_url(url_prefix: str) -> str:
+                return f"{url_prefix}/patches/ssrf2/submit_api_url/"
 
 
 class State(Enum):
@@ -110,7 +124,7 @@ def check_response(
     return success
 
 
-def test_static_routes() -> list[bool]:
+def test_static_routes(url_prefix: str, verify: bool) -> list[bool]:
     base_url = f"{url_prefix}"
     health_url = f"{url_prefix}/health"
     r = requests.get(base_url, verify=verify)
@@ -132,7 +146,9 @@ def test_static_routes() -> list[bool]:
     return [r1, r2]
 
 
-def test_submission(challenge: Passphrases, secret: str) -> bool:
+def test_submission(
+    challenge: Passphrases, secret: str, *, url_prefix: str, verify: bool
+) -> bool:
     url = f"{url_prefix}/submission"
     data = {"secret": secret, "challenge": challenge.name}
     r = requests.post(url, json=data, verify=verify)
@@ -145,7 +161,7 @@ def test_submission(challenge: Passphrases, secret: str) -> bool:
     )
 
 
-def sqli_login_bypass(state: State) -> list[bool]:
+def sqli_login_bypass(state: State, *, url_prefix: str, verify: bool) -> list[bool]:
     expected_response: str | dict[str, str]
     username = "administrator"
     password = "' OR 'a' = 'a"
@@ -153,10 +169,12 @@ def sqli_login_bypass(state: State) -> list[bool]:
 
     match state:
         case State.VULNERABLE:
-            url, status_code = SQLI.LoginBypass.Vulnerabilities.expected_data_for_login
+            url, status_code = SQLI.LoginBypass.Vulnerabilities.get_login_data(
+                url_prefix
+            )
         case State.PATCHED:
             url, status_code, patched_expected_response = (
-                SQLI.LoginBypass.Patches.expected_data_for_login
+                SQLI.LoginBypass.Patches.get_login_data(url_prefix)
             )
         case _:
             assert_never(state)
@@ -182,23 +200,25 @@ def sqli_login_bypass(state: State) -> list[bool]:
     responses = [response]
 
     if state == State.VULNERABLE:
-        submission_response = test_submission(Passphrases.sqli1, actual_response)
+        submission_response = test_submission(
+            Passphrases.sqli1, actual_response, url_prefix=url_prefix, verify=verify
+        )
         responses.append(submission_response)
 
     return responses
 
 
-def sqli_second_order(state: State) -> list[bool]:
+def sqli_second_order(state: State, *, url_prefix: str, verify: bool) -> list[bool]:
     password = str(uuid.uuid4())
 
     match state:
         case State.VULNERABLE:
             url, status_code, expected_response = (
-                SQLI.SecondOrder.Vulnerabilities.expected_data_for_register
+                SQLI.SecondOrder.Vulnerabilities.get_register_data(url_prefix)
             )
         case State.PATCHED:
             url, status_code, expected_response = (
-                SQLI.SecondOrder.Patches.expected_data_for_register
+                SQLI.SecondOrder.Patches.get_register_data(url_prefix)
             )
         case _:
             assert_never(state)
@@ -217,11 +237,11 @@ def sqli_second_order(state: State) -> list[bool]:
     match state:
         case State.VULNERABLE:
             url, status_code = (
-                SQLI.SecondOrder.Vulnerabilities.expected_data_for_change_password
+                SQLI.SecondOrder.Vulnerabilities.get_change_password_data(url_prefix)
             )
         case State.PATCHED:
             url, status_code, patched_expected_response = (
-                SQLI.SecondOrder.Patches.expected_data_for_change_password
+                SQLI.SecondOrder.Patches.get_change_password_data(url_prefix)
             )
         case _:
             assert_never(state)
@@ -249,20 +269,22 @@ def sqli_second_order(state: State) -> list[bool]:
     responses = [first_check, second_check]
 
     if state == State.VULNERABLE:
-        submission_response = test_submission(Passphrases.sqli2, actual_response)
+        submission_response = test_submission(
+            Passphrases.sqli2, actual_response, url_prefix=url_prefix, verify=verify
+        )
         responses.append(submission_response)
 
     return responses
 
 
-def ssrf_webhook(state: State) -> list[bool]:
+def ssrf_webhook(state: State, *, url_prefix: str, verify: bool) -> list[bool]:
     submission_secret: str
     match state:
         case State.VULNERABLE:
-            url = SSRF.Webhook.Vulnerabilities.submit_webhook_url
+            url = SSRF.Webhook.Vulnerabilities.get_submit_webhook_url(url_prefix)
             file_name = "backend/tests/ssrf_webhook_test_urls_vulnerable.json"
         case State.PATCHED:
-            url = SSRF.Webhook.Patches.submit_webhook_url
+            url = SSRF.Webhook.Patches.get_submit_webhook_url(url_prefix)
             file_name = "backend/tests/ssrf_webhook_test_urls_patched.json"
         case _:
             assert_never(state)
@@ -310,13 +332,17 @@ def ssrf_webhook(state: State) -> list[bool]:
     # vulnerable endpoint (since that will confirm that the challenge is working
     # correctly)
     if state == State.VULNERABLE:
-        submission_response = test_submission(Passphrases.ssrf1, submission_secret)
+        submission_response = test_submission(
+            Passphrases.ssrf1, submission_secret, url_prefix=url_prefix, verify=verify
+        )
         all_response_checks.append(submission_response)
 
     return all_response_checks
 
 
-def ssrf_local_file_inclusion(state: State) -> list[bool]:
+def ssrf_local_file_inclusion(
+    state: State, *, url_prefix: str, verify: bool
+) -> list[bool]:
     submission_secret: str
     cat_coin_price_pattern = re.compile(
         r"Price at \d{2}:\d{2}:\d{2}\.\d{6} - \$\d+\.\d+"
@@ -324,10 +350,10 @@ def ssrf_local_file_inclusion(state: State) -> list[bool]:
 
     match state:
         case State.VULNERABLE:
-            url = SSRF.LocalFileInclusion.Vulnerabilities.submit_api_url_url
+            url = SSRF.LocalFileInclusion.Vulnerabilities.get_submit_api_url(url_prefix)
             file_name = "backend/tests/ssrf_lfi_test_urls_vulnerable.json"
         case State.PATCHED:
-            url = SSRF.LocalFileInclusion.Patches.submit_api_url_url
+            url = SSRF.LocalFileInclusion.Patches.get_submit_api_url(url_prefix)
             file_name = "backend/tests/ssrf_lfi_test_urls_patched.json"
         case _:
             assert_never(state)
@@ -377,36 +403,79 @@ def ssrf_local_file_inclusion(state: State) -> list[bool]:
             break
 
     if state == State.VULNERABLE:
-        submission_response = test_submission(Passphrases.ssrf2, submission_secret)
+        submission_response = test_submission(
+            Passphrases.ssrf2, submission_secret, url_prefix=url_prefix, verify=verify
+        )
         all_response_checks.append(submission_response)
 
     return all_response_checks
 
 
-start_time = round(time.time() * 1000)
-print("Starting functional test...\n\n")
-results = []
+def get_cli_arguments() -> str:
+    opts = [opt for opt in sys.argv[1:] if opt.startswith("-")]
+    assert len(opts) == 1, "Only one flag can be provided"
+    message = "Overriding default: "
 
-print("Testing static routes...")
-results.extend(test_static_routes())
+    if "-v" in opts:
+        verify = True
+        print(f"{message} using verified requests")
+    else:
+        verify = False
+        print(f"{message} using unverified requests")
 
-for state in State:
-    print(f"Testing {state.name} state for SQLi login bypass...")
-    results.extend(sqli_login_bypass(state))
+    if "-r" in opts:
+        url_prefix = REMOTE_SERVER_URL
+        print(f"{message} using remote server")
+    elif "-l" in opts:
+        url_prefix = LOCAL_SERVER_URL
+        print(f"{message} using local server")
+    elif "-c" in opts:
+        args = [arg for arg in sys.argv[1:] if not arg.startswith("-")]
+        if not args:
+            raise ValueError("Custom URL must be provided with -c flag")
+        custom_url = args[0]
+        url_prefix = (
+            custom_url if custom_url.startswith("http") else f"https://{custom_url}"
+        )
+        print(f"{message} using custom server {url_prefix}")
+    else:
+        raise ValueError("Invalid flag provided")
 
-    print(f"Testing {state.name} state for SQLi second order...")
-    results.extend(sqli_second_order(state))
+    return url_prefix, verify
 
-    print(f"Testing {state.name} state for SSRF webhook...")
-    results.extend(ssrf_webhook(state))
 
-    print(f"Testing {state.name} state for SSRF local file inclusion...")
-    results.extend(ssrf_local_file_inclusion(state))
+if __name__ == "__main__":
+    url_prefix, verify = get_cli_arguments()
+    start_time = round(time.time() * 1000)
+    print("Starting functional test...\n\n")
+    results = []
 
-stop_time = round(time.time() * 1000)
-run_time = (stop_time - start_time) / 1000
-print(
-    f"\n\nFinished running {len(results)} functional tests. Run time: {run_time} seconds"
-)
+    if not verify:
+        # Disable loud warnings if not verifying TLS certificate
+        urllib3.disable_warnings()
 
-assert all(results), "There were failed tests 😭"
+    print("Testing static routes...")
+    results.extend(test_static_routes(url_prefix=url_prefix, verify=verify))
+
+    for state in State:
+        print(f"Testing {state.name} state for SQLi login bypass...")
+        results.extend(sqli_login_bypass(state, url_prefix=url_prefix, verify=verify))
+
+        print(f"Testing {state.name} state for SQLi second order...")
+        results.extend(sqli_second_order(state, url_prefix=url_prefix, verify=verify))
+
+        print(f"Testing {state.name} state for SSRF webhook...")
+        results.extend(ssrf_webhook(state, url_prefix=url_prefix, verify=verify))
+
+        print(f"Testing {state.name} state for SSRF local file inclusion...")
+        results.extend(
+            ssrf_local_file_inclusion(state, url_prefix=url_prefix, verify=verify)
+        )
+
+    stop_time = round(time.time() * 1000)
+    run_time = (stop_time - start_time) / 1000
+    print(
+        f"\n\nFinished running {len(results)} functional tests. Run time: {run_time} seconds"
+    )
+
+    assert all(results), "There were failed tests 😭"
